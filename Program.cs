c462c2ca5c4d2f51d1375c273588bdd2478cd59f
yanofb <73286248+yanofb@users.noreply.github.com>
2021-10-25 22:21:15 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -63,7 +63,7 @@ namespace DIO.Usuarios
 										
         private static void AtualizarUsuario()										
 		{								
-			Console.Write("Digite o id da série: ");							
+			Console.Write("Digite o id do usuário: ");							
 			int indiceUsuario = int.Parse(Console.ReadLine());							
 										
 			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1							
@@ -100,7 +100,7 @@ namespace DIO.Usuarios
 										
 			if (lista.Count == 0)							
 			{							
-				Console.WriteLine("Nenhuma série cadastrada.");						
+				Console.WriteLine("Nenhum usuário cadastrado.");						
 				return;						
 			}							
 										
@@ -113,48 +113,18 @@ namespace DIO.Usuarios
 		}								
 										
         private static void InserirUsuario()										
-		{								
-			Console.WriteLine("Inserir nova série");							
-										
-			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getvalues?view=netcore-3.1							
-			// https://docs.microsoft.com/pt-br/dotnet/api/system.enum.getname?view=netcore-3.1							
-			foreach (int i in Enum.GetValues(typeof(Genero)))							
-			{							
-				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));						
-			}							
-			Console.Write("Digite o gênero entre as opções acima: ");							
-			int entradaGenero = int.Parse(Console.ReadLine());							
-										
-			Console.Write("Digite o Título da Série: ");							
-			string entradaTitulo = Console.ReadLine();							
-										
-			Console.Write("Digite o Ano de Início da Série: ");							
-										
-			int entradaAno = int.Parse(Console.ReadLine());							
-										
-			Console.Write("Digite a Descrição da Série: ");							
-			string entradaDescricao = Console.ReadLine();							
-										
-			Usuario novaUsuario = new Usuario(id: repositorio.ProximoId(),							
-										genero: (Genero)entradaGenero,
-										titulo: entradaTitulo,
-										ano: entradaAno,
-										descricao: entradaDescricao);
-										
-			repositorio.Insere(novaUsuario);							
-		}								
-										
+							
         private static string ObterOpcaoUsuario()										
 		{								
 			Console.WriteLine();							
 			Console.WriteLine("DIO Séries a seu dispor!!!");							
 			Console.WriteLine("Informe a opção desejada:");							
 										
-			Console.WriteLine("1- Listar séries");							
-			Console.WriteLine("2- Inserir nova série");							
-			Console.WriteLine("3- Atualizar série");							
-			Console.WriteLine("4- Excluir série");							
-			Console.WriteLine("5- Visualizar série");							
+			Console.WriteLine("1- Listar usuários");							
+			Console.WriteLine("2- Inserir novo usuários");							
+			Console.WriteLine("3- Atualizar usuários");							
+			Console.WriteLine("4- Excluir usuários");							
+			Console.WriteLine("5- Visualizar usuários");							
 			Console.WriteLine("C- Limpar Tela");							
 			Console.WriteLine("X- Sair");							
 			Console.WriteLine();							
